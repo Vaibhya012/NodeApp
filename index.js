e9fd7b176e1a5dddc97a8508b1b9021b2dec09e1
Vaibhya012 <gayakwadvaibhav120@gmail.com>
2025-01-12 14:39:24 +0530

    Node App 4

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('nahi disla ');
+ res.send('aata disla ka?');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
 module.exports = app;
-=======
-var express = require('express');
- 
-var app = express();//Respond with "hello world" for requests that hit our root "/"
-app.get('/', function (req, res) {
- res.send('nahi disla ');
-});//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
- 
-module.exports = app;
->>>>>>> 7df0eff773036ddcc0b8b2986370e62fde3b7dec
